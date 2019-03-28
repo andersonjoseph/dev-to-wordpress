16d50faf3f9b292d3a0429947b6da531bb2d8f5f
andersonjoseph <andersonjoseph@mailfence.com>
2019-03-28 13:54:21 -0400

    remove user id field

--- public/widget.php
+++ public/widget.php
@@ -1,7 +1,7 @@
 <div id="DTW_widget">
   <h4 class="widget-title"><?php echo $instance['title']; ?></h4>
   
-  <div class="DTW-container" data-n="<?php echo $instance['num_of_posts']?>" data-userID="<?php echo $instance['user_id']?>">
+  <div class="DTW-container" data-n = "<?php echo $instance['num_of_posts']?>" data-username = "<?php echo $instance['username']?>">
     <div style="text-align: center;">
       <div class="lds-dual-ring"></div>
     </div>

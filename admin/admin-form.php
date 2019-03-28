f27a2b97b7f3d0db40fe988b54178166d3b2080d
andersonjoseph <andersonjoseph@mailfence.com>
2019-03-28 13:51:55 -0400

    remove getID field

--- admin/admin-form.php
+++ admin/admin-form.php
@@ -1,4 +1,4 @@
-<div id="<?php echo 'form_' . $this->get_field_id('title'); ?>">
+<div>
   <div>
   <label for="<?php echo esc_attr( $this->get_field_id('title')); ?>">Title</label>
   <input 
@@ -19,18 +19,6 @@
       placeholder="samsepi0l"
       style="width: 40%;"
     />
-    <button id="<?php echo 'button_' . $this->get_field_id('title'); ?>" class="button button-primary" data-formId = "<?php echo 'form_' . $this->get_field_id('title'); ?>">Get ID</button>
-  </div>
-  <br>
-  <div id="userid-container">
-    <label for="<?php echo esc_attr( $this->get_field_id('user_id')); ?>">User ID</label>
-    <input 
-      type="text" 
-      id="<?php echo esc_attr( $this->get_field_id('user_id')); ?>"
-      name="<?php echo esc_attr($this->get_field_name('user_id')); ?>" 
-      value="<?php echo esc_attr($user_id); ?>"
-      readonly=""
-    />
   </div>
   <br>
   <div>
@@ -40,11 +28,9 @@
       id="<?php echo esc_attr( $this->get_field_id('num_of_posts')); ?>"
       name="<?php echo esc_attr($this->get_field_name('num_of_posts')); ?>" 
       value="<?php echo esc_attr($num_of_posts); ?>"
+      max = 30
+      min = 1
     />
   </div>
   <br>
 </div>
-<script type="text/javascript">
-  window.DTW_getIDButton = document.getElementById('<?php echo 'button_' . $this->get_field_id('title'); ?>')
-  DTW_setButtonEvent(DTW_getIDButton);
-</script>
